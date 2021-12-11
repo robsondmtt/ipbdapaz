9b3ecf3e659a01a350e1d313be3fd7de4c237559
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2021-12-11 11:54:00 -0300

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@chakra-ui/button'
-import { Heading } from '@chakra-ui/react'
+import { Box, Center, Container, Flex, Heading } from '@chakra-ui/react'
 import Head from 'next/head'
 import Image from 'next/image'
 import LayoutCenter from '../components/Layout/LayoutCenter'
@@ -16,14 +16,29 @@ export default function Home() {
   return (
 
     <>
-    <Navbar />  
-      <LayoutCenter>
-        <Heading>
-          Bem vindo!
-
-        </Heading>
-        
-      </LayoutCenter>
+      <Navbar />
+      {/* <LayoutCenter> */}
+      <Container maxW='container.xl'>
+        <Center>
+          <Heading>
+            Bem vindo!
+          </Heading>
+        </Center>
+        <Center>
+          <Flex
+            maxW={'1000px'}
+            direction={['column', 'column', 'row', 'row']}
+            justifyContent={'space-around'}
+            alignItems={'center'}
+            justify={'center'}
+            w={['90vw', '90vw', '70vw']}>
+            <Flex m={'2'} align={'center'} w={'full'} bg="green.300">A</Flex>
+            <Flex m={'2'} align={'center'} w={'full'} bg="green.500">A</Flex>
+            <Flex m={'2'} align={'center'} w={'full'} bg="green.700">A</Flex>
+          </Flex>
+        </Center>
+      </Container>
+      {/* </LayoutCenter> */}
     </>
   )
 }
