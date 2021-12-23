2954973c4f5615507f4bb4a58d24e9ca7249ed22
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2021-12-23 15:33:50 -0300

    update painel

--- pages/tesouraria/painel.tsx
+++ pages/tesouraria/painel.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+import { Box, Flex, Grid, Heading, SimpleGrid, Text, useMediaQuery } from "@chakra-ui/react"
 
 interface PainelProps {
     receita: any;
@@ -6,7 +6,8 @@ interface PainelProps {
 }
 const Painel = (props: PainelProps) => {
 
-        
+    const [isLargerThan1280] = useMediaQuery('(min-width: 600px)')
+
     const totalReceita = props.receita.reduce(getTotalReceita, 0)
     function getTotalReceita(total, item) {
         return total + item.valor
@@ -17,43 +18,53 @@ const Painel = (props: PainelProps) => {
     }
 
     return (
-        <Flex w="full" >
-            <Box w="100%" mx={["1", "4"]} align="center" alignItems={'center'}>
-                <Box bg="blue.500" color="white" borderRadius="lg" py="2" my="6">
-                    <Heading size="md">Receitas</Heading>
-                    <Text>
-                        {
-                            totalReceita ?
-                            totalReceita.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) 
-                                : (0).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
-                        }
-                    </Text>
-                </Box>
+        <>
+            <Box>
+                <Box width={['50%', '50%']} bg="red">a</Box>
+                <Box width={['50%', '50%']} bg="green">b</Box>
+                <Box width={['100%','100%']} bg="teal">c</Box>
             </Box>
-            <Box w="100%" mx={["1", "4"]} align="center" alignItems={'center'}>
-                <Box bg="red.500" color="white" borderRadius="lg" py="2" my="6">
-                    <Heading size="md">Despesas</Heading>
-                    <Text>
-                        {
-                            totalDespesa ?
-                            totalDespesa.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) 
-                            : (0).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
-                        }
-                    </Text>
+
+
+
+            <Flex w="full" >
+                <Box w="100%" mx={["1", "1"]} align="center" alignItems={'center'}>
+                    <Box bg="blue.500" color="white" borderRadius="lg" py="2" my="6">
+                        <Text fontSize={[20,22]}><strong>Receitas</strong></Text>
+                        <Text fontSize={[14,16]}>
+                            {
+                                totalReceita ?
+                                    totalReceita.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+                                    : (0).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+                            }
+                        </Text>
+                    </Box>
                 </Box>
-            </Box>
-            <Box w="100%" mx={["1", "4"]} align="center" alignItems={'center'}>
-                <Box bg="green.500" color="white" borderRadius="lg" py="2" my="6">
-                    <Heading size="md">Saldo</Heading>
-                    <Text>
-                        {
-                        (totalReceita - totalDespesa)
-                        .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
-                        }
-                    </Text>
+                <Box w="100%" mx={["1", "1"]} align="center" alignItems={'center'}>
+                    <Box bg="red.500" color="white" borderRadius="lg" py="2" my="6">
+                        <Text fontSize={[20,22]}><strong>Despesas</strong></Text>
+                        <Text fontSize={[14,16]}>
+                            {
+                                totalDespesa ?
+                                    totalDespesa.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+                                    : (0).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+                            }
+                        </Text>
+                    </Box>
                 </Box>
-            </Box>
-        </Flex>
+                <Box w="100%" mx={["1", "1"]} align="center" alignItems={'center'}>
+                    <Box bg="green.500" color="white" borderRadius="lg" py="2" my="6">
+                        <Text fontSize={[20,22]}><strong>Saldo</strong></Text>
+                        <Text fontSize={[14,16]}>
+                            {
+                                (totalReceita - totalDespesa)
+                                    .toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
+                            }
+                        </Text>
+                    </Box>
+                </Box>
+            </Flex>
+        </>
     )
 }
 
