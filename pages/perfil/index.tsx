77ab0812669954fbf8f6deef73b1fb9d43ca6048
Robson Amorim <92536182+robsondmtt@users.noreply.github.com>
2021-12-24 15:04:39 -0300

    perfil

--- pages/perfil/index.tsx
+++ pages/perfil/index.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { auth, criarAdministrador, criarConselho, eliminarAdministrador, eliminarConselho } from '../../lib/firebase'
 import { getFirestore, collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
-import { Button, Center, List, Box, Switch } from "@chakra-ui/react";
+import { Button, Center, List, Box, Switch, Flex, Text, Image, Badge } from "@chakra-ui/react";
 import useAuth from "../../hooks/useAuth";
 import Loading from "../../components/Loading";
 import Navbar from "../../components/Nav/Navbar";
 import LayoutContent from "../../components/Layout/LayoutContent";
-// import { Box } from "@chakra-ui/react";
-
 
 const Perfil = () => {
 
@@ -25,7 +23,25 @@ const Perfil = () => {
     const user = auth.currentUser
     console.log(user.email);
 
-    // const [switchAdm,setSwitchAdm] = useState(false)
+    const toggleAdministrador = (nivel, email, uid) => {
+        if (nivel === 'adm') {
+            eliminarAdministrador({ email: email, uid: uid })
+        } else {
+            criarAdministrador({ email: email, uid: uid })
+        }
+
+    }
+    const toggleConselho = (nivel, email, uid) => {
+        if (nivel === 'conselho') {
+            eliminarConselho({ email: email, uid: uid })
+        } else {
+            criarConselho({ email: email, uid: uid })
+        }
+
+    }
+
+
+
 
     return (
         <>
@@ -44,22 +60,54 @@ const Perfil = () => {
 
                             Collection:{' '}
                             {users.docs.map((doc) => (
+
                                 <Box key={doc.id} bg="gray.100" borderRadius="lg" my="4" p="4">
+                                    <Box justifyContent="space-between">
+                                        <Center>
+                                           <strong> {doc.data().displayName}</strong>
+                                        </Center>
+                                        <Flex justifyContent="space-around" my="6">
+                                            <Text mb='0'>
+                                                Admin
+                                            </Text>
+                                            <Switch defaultChecked={
+                                                doc.data().nivelPermissao === 'adm' ? true : false
+                                            } id='email-alerts'
+                                                onChange={
+                                                    () => toggleAdministrador(
+                                                        doc.data().nivelPermissao, doc.data().email, doc.id
+                                                    )} />
+                                            <Text mb='0'>
+                                                Conselho
+                                            </Text>
+                                            <Switch defaultChecked={
+                                                doc.data().nivelPermissao === 'conselho' ? true : false
+                                            } id='email-alerts'
+                                                onChange={
+                                                    () => toggleConselho(
+                                                        doc.data().nivelPermissao, doc.data().email, doc.id
+                                                    )} />
+                                            <Text mb='0'>
+                                                Convidado
+                                            </Text>
+                                            <Switch defaultChecked={
+                                                doc.data().nivelPermissao === 'convidado' ? true : false
+                                            } id='email-alerts' isDisabled colorScheme="blue"
+                                            />
+
+
+                                        </Flex>
+                                    </Box>
+
+
+
+
                                     <Center>
-                                        {doc.data().email}
+                                        <Badge p="2" variant='outline' colorScheme='green'>
+                                            <strong> {doc.data().nivelPermissao}</strong>
+                                        </Badge>
                                     </Center>
-                                    <Center>
-                                        {doc.data().nivelPermissao}
-                                    </Center>
-                                    <Center mb="2">
-                                        {/* <Switch defaultChecked={switchAdm} onchange={setSwitchAdm} colorScheme='red' /> */}
-                                        <Button bg="green.300" mr="1" onClick={() => criarAdministrador({ email: doc.data().email, uid: doc.id })} >Criar Administrador</Button>
-                                        <Button bg="red.400" ml="1" onClick={() => eliminarAdministrador({ email: doc.data().email, uid: doc.id })} >Eliminar Administrador</Button>
-                                    </Center>
-                                    <Center mb="2">
-                                        <Button bg="green.300" mr="1" onClick={() => criarConselho({ email: doc.data().email, uid: doc.id })} >Criar Conselho</Button>
-                                        <Button bg="red.400" ml="1" onClick={() => eliminarConselho({ email: doc.data().email, uid: doc.id })} >Eliminar Conselho</Button>
-                                    </Center>
+
 
                                 </Box>
 
